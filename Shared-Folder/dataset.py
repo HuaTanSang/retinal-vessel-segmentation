e56ccdee2b4f61a139a3a07104e3970dbba1b9a8
HuaTanSang <huatansang2004@gmail.com>
2025-04-14 20:44:04 +0700

    create hrf dataset

--- Shared-Folder/dataset.py
+++ Shared-Folder/dataset.py
@@ -4,54 +4,36 @@ from torchvision import transforms
 from PIL import Image
 import glob
 
-class BrainTumorDataset(Dataset):
-    def __init__(self, root_folder):
-        self.image_dir = os.path.join(root_folder, "image")
-        self.mask_dir = os.path.join(root_folder, "mask")
+class HRF_Dataset(Dataset): 
+    def __init__(self, root_folder): 
+        self.root_folder = root_folder 
 
-        list_subfolder = ["0", "1", "2", "3"]
-        self.image_files = [] 
-        self.mask_files = [] 
+        self.images = sorted(
+            glob.glob(os.path.join(self.root_folder, 'images/**.jpg')) +
+            glob.glob(os.path.join(self.root_folder, 'images/**.JPG'))
+        )
+        self.masks = sorted(glob.glob(self.root_folder + 'manual1/**.tif'))
 
-        for sub in list_subfolder: 
-            image_paths = glob.glob(os.path.join(self.image_dir, sub, "*.jpg"))
-            mask_paths = glob.glob(os.path.join(self.mask_dir, sub, "*.jpg"))
+        assert len(self.images == self.images), "Number of mask and image mis-match!"
 
-            image_paths.sort()
-            mask_paths.sort()
-
-            self.image_files.extend(image_paths)
-            self.mask_files.extend(mask_paths)
-
-        assert len(self.image_files) == len(self.mask_files), "Số lượng ảnh và mask không khớp"
-
-        self.image_transform = transforms.Compose([
-            transforms.Resize((32, 32)),
+        self.transforms_img = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Lambda(lambda x: x.expand(3, -1, -1) if x.shape[0] == 1 else x),  
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+            transforms.Resize((256,256))
         ])
 
-        self.mask_transform = transforms.Compose([
-            transforms.Resize((32, 32)),
-            transforms.ToTensor()
+        self.transforms_masks = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((256,256),interpolation=transforms.InterpolationMode.NEAREST)
         ])
 
-    def __len__(self):
-        return len(self.image_files)
-
+    def __len__(self): 
+        return len(self.images)
+    
     def __getitem__(self, index):
-        image_path = self.image_files[index]
-        mask_path = self.mask_files[index]
-
-        image = Image.open(image_path).convert("RGB") 
-        image = self.image_transform(image)
-
-        mask = Image.open(mask_path).convert("L")  
-        mask = self.mask_transform(mask)
-        mask = (mask > 0).float() 
+        image = self.transforms_img(Image.open(self.images[index]).convert('RGB'))
+        mask = self.transforms_masks(Image.open(self.masks[index]).convert('L'))
         
         return {
-            "image": image,
-            "mask": mask
+            "image" : image, 
+            "mask" : mask
         }
